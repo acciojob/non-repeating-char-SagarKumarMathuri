2938fce94c52c032509f21aa621ada5a1c35aaec
sagar kumar mathuri <sagarku2002@gmail.com>
2026-04-22 10:53:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+// function firstNonRepeatedChar(str) {
+//  // Write your code here
+// }
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
+
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const count = {};
+
+  // Count frequency of each character
+  for (let char of str) {
+    count[char] = (count[char] || 0) + 1;
+	  
+  }
+
+  // Find first non-repeating character
+  for (let char of str) {
+    if (count[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
